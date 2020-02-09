cbb925ef14ffae125d640f34337c3b45e09eb3f3
Hugoyu-ops <59141842+Hugoyu-ops@users.noreply.github.com>
2020-02-10 02:48:18 +0800

    'added_completed_and_completed's_modal'

--- poonchoi/urls.py
+++ poonchoi/urls.py
@@ -6,4 +6,12 @@ urlpatterns = [
     path('edit/<str:order_id>', views.edit, name='edit'),
     path('delete/<str:order_id>', views.delete, name='delete'),
     path('Date/', views.search_by_date, name='date'),
+    path('edit_date/<str:order_id>', views.edit_on_date, name='edit_date'),
+    path('delete_date/<str:order_id>', views.delete_on_date, name='delete_date'),
+    path('completed/', views.completed, name='completed'),
+    path('Date_completed/', views.completed_by_date, name='date_on_completed'),
+    path('completed_edit_date/<str:order_id>', views.edit_on_date_completed, name='completed_edit_date'),
+    path('completed_delete_date/<str:order_id>', views.completed_delete_on_date, name='completed_deleted_date'),
+    path('completed_delete/<str:order_id>', views.completed_delete, name='completed_deleted'),
+    path('completed_edit/<str:order_id>', views.edit_completed, name='completed_edit'),
 ]
--- poonchoi/views.py
+++ poonchoi/views.py
@@ -14,7 +14,7 @@ AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID', AIRTABLE_MOVIESTABL
 # Create your views here.
 def home(request):
     user_query = request.GET.get('query','')
-    search = AT.get_all(formula="FIND('" + user_query + "', {Tel1})")
+    search = AT.get_all(formula="FIND('" + user_query + "', {Tel1})", sort='Dates')
     date_query = str(request.GET.get('date_query'))
     date = AT.get_all(formula="FIND('" + date_query + "', {Dates})")
     context = {
@@ -22,6 +22,17 @@ def home(request):
         'date': date,
     }
     return render(request, 'home.html', context)
+
+def completed(request):
+    user_query = request.GET.get('query','')
+    search = AT.get_all(formula="FIND('" + user_query + "', {Tel1})", sort='Dates')
+    date_query = str(request.GET.get('date_query'))
+    date = AT.get_all(formula="FIND('" + date_query + "', {Dates})")
+    context = {
+        'search':search,
+        'date': date,
+    }
+    return render(request, 'completed.html', context)
 
 def createoreder(request):
     if request.method == 'POST':
@@ -45,6 +56,8 @@ def detail(request, order_id):
     order = AT.get(order_id)
     return render(request, 'detail.html', order)
 
+
+# edit button on home page
 def edit(request, order_id):
     if request.method == 'POST':
         data = {
@@ -60,13 +73,84 @@ def edit(request, order_id):
             'Price': int(request.POST.get('price')),
             'Completed': request.POST.get('complete'),
         }
-
         AT.update(order_id, data)
     return redirect('/')
+
+# edit button on search by date page
+def edit_on_date(request, order_id):
+    if request.method == 'POST':
+        date_para = AT.get(order_id)['fields'].get('Dates')
+        data = {
+            'Name': request.POST.get('name'),
+            'Tel1': request.POST.get('tel1'),
+            'Tel2': request.POST.get('tel2'),
+            'Districts': request.POST.get('district'),
+            'Address': request.POST.get('address'),
+            'Dates': request.POST.get('date'),
+            'time': str(request.POST.get('time')),
+            'AMPM': request.POST.get('AMPM'),
+            'Quan': int(request.POST.get('quan')),
+            'Price': int(request.POST.get('price')),
+            'Completed': request.POST.get('complete'),
+        }
+        AT.update(order_id, data)
+    return redirect('/Date/?date_query='+date_para)
+
+def edit_on_date_completed(request, order_id):
+    if request.method == 'POST':
+        date_para = AT.get(order_id)['fields'].get('Dates')
+        data = {
+            'Name': request.POST.get('name'),
+            'Tel1': request.POST.get('tel1'),
+            'Tel2': request.POST.get('tel2'),
+            'Districts': request.POST.get('district'),
+            'Address': request.POST.get('address'),
+            'Dates': request.POST.get('date'),
+            'time': str(request.POST.get('time')),
+            'AMPM': request.POST.get('AMPM'),
+            'Quan': int(request.POST.get('quan')),
+            'Price': int(request.POST.get('price')),
+            'Completed': request.POST.get('complete'),
+        }
+        AT.update(order_id, data)
+    return redirect('/Date_completed/?date_query='+date_para)
+
+def edit_completed(request, order_id):
+    if request.method == 'POST':
+        date_para = AT.get(order_id)['fields'].get('Dates')
+        data = {
+            'Name': request.POST.get('name'),
+            'Tel1': request.POST.get('tel1'),
+            'Tel2': request.POST.get('tel2'),
+            'Districts': request.POST.get('district'),
+            'Address': request.POST.get('address'),
+            'Dates': request.POST.get('date'),
+            'time': str(request.POST.get('time')),
+            'AMPM': request.POST.get('AMPM'),
+            'Quan': int(request.POST.get('quan')),
+            'Price': int(request.POST.get('price')),
+            'Completed': request.POST.get('complete'),
+        }
+        AT.update(order_id, data)
+    return redirect('/completed')
 
 def delete(request, order_id):
     AT.delete(order_id)
     return redirect("/")
+
+def completed_delete(request, order_id):
+    AT.delete(order_id)
+    return redirect("/completed")
+
+def delete_on_date(request, order_id):
+    date_para = AT.get(order_id)['fields'].get('Dates')
+    AT.delete(order_id)
+    return redirect('/Date/?date_query='+date_para)
+
+def completed_delete_on_date(request, order_id):
+    date_para = AT.get(order_id)['fields'].get('Dates')
+    AT.delete(order_id)
+    return redirect('/Date_completed/?date_query='+date_para)
 
 def search_by_date(request):
     if request.method == "GET":
@@ -80,7 +164,7 @@ def search_by_date(request):
         #AT.get_all()
         date_query = str(request.GET.get('date_query', ''))
         #date = AT.search('Dates', date_query)
-        date = AT.get_all(formula="FIND('" + date_query + "', {Dates})")
+        date = AT.get_all(formula="FIND('" + date_query + "', {Dates})", sort=['AMPM', 'time'])
         for order in date:
             amount.append(int(order['fields']['total_price']))
             total_amount = sum(amount)
@@ -145,3 +229,16 @@ def search_by_date(request):
         }
 
     return render(request, 'search_by_date.html', context)
+
+def completed_by_date(request):
+    if request.method == "GET":
+        #AT.get_all()
+        date_query = str(request.GET.get('date_query', ''))
+        #date = AT.search('Dates', date_query)
+        date = AT.get_all(formula="FIND('" + date_query + "', {Dates})", sort=['AMPM', 'time'])
+
+        context = {
+            'date': date,
+        }
+
+    return render(request, 'completed_by_date.html', context)
